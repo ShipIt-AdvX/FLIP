6dbfd40d7e54e24e76c57b06449f2ca6f1191587
littlekan233 <littlekan233@qq.com>
2026-07-26 05:34:25 +0800

    delete api temp

--- api/index.js
+++
@@ -1,7 +0,0 @@
-import fastify from "../packages/backend/src/core";
-
-export default async function handler(req, res) {
-    await fastify.ready();
-
-    fastify.server.emit("request", req, res);
-}
